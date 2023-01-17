63eefbb456dcf693189160dfae93c2458c6f383a
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-01-17 15:50:19 +0900

    emaillist02 #1

---
+++ emaillist02/src/main/java/com/douzone/emaillist/controller/EmaillistServlet.java
@@ -0,0 +1,42 @@
+package com.douzone.emaillist.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.emaillist.dao.EmaillistDao;
+import com.douzone.emaillist.vo.EmaillistVo;
+
+
+public class EmaillistServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		if("form".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);
+			
+		} else {
+			//다른 action입력시 default 화면
+			List<EmaillistVo> list = new EmaillistDao().findAll();
+			//servlet과 index.jsp가 가진 request 객체를 사용해서 Map에 list로 넣기
+			request.setAttribute("list", list);
+			//request를 jsp로 연장
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response); //클라이언트의 요청을 jsp로 연장
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
